e8eb2e3a3297aec0985899c391ac7b92e483c6f3
Satvik Sharma <satvik.sharma2@gmail.com>
2019-09-08 22:41:59 +1000

    More content

--- components/Title.js
+++ components/Title.js
@@ -4,12 +4,12 @@ import { useSteps } from "mdx-deck";
 const CONTENT = [
     "How I Learned to Stop Worrying and Love the Bomb",
     <>How I Learned to Stop Worrying and Love <s>the Bomb</s></>,
-    "How I Learned to Stop Worrying and Love Component Testing",
+    <>How I Learned to Stop Worrying and Love <s>the Bomb</s> Component Testing</>,
 
 ]
 
 export default () => {
     const step = useSteps(2);
 
-    return CONTENT[step];
+    return <h1>{CONTENT[step]}</h1>;
 }
