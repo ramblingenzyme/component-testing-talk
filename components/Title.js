db347737d3db028757f2c5a08b6ff38001850fb3
Satvik Sharma <satvik@easyagile.com>
2019-10-04 08:55:31 +1000

    Make title static

--- components/Title.js
+++ components/Title.js
@@ -4,12 +4,17 @@ import { useSteps } from "mdx-deck";
 const CONTENT = [
     "How I Learned to Stop Worrying and Love the Bomb",
     <>How I Learned to Stop Worrying and Love <s>the Bomb</s></>,
-    <>How I Learned to Stop Worrying and Love <s>the Bomb</s> Component Testing</>,
+    <></>,
 
 ]
 
 export default () => {
     const step = useSteps(2);
 
-    return <h1 style={{ marginLeft: "20px" }} >{CONTENT[step]}</h1>;
+    return (
+        <h1 style={{ marginLeft: "20px" }} >
+            How I Learned to Stop Worrying and Love <s>the Bomb</s> Component Testing
+        </h1>
+    );
+
 }
